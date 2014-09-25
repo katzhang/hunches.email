132247c67bec83bae6072d6564de86d9eb82ec2e
katzhang <arcadychang@gmail.com>
2014-09-24 23:06:16 -0400

    added javascript client library test

---
+++ test/javascript/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gmailApiTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gmailApiTestApp
+ */
+angular.module('gmailApiTestApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ test/javascript/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gmailApiTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gmailApiTestApp
+ */
+angular.module('gmailApiTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
