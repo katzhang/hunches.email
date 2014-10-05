127b72d2181f8eb51277de406870f85d4777b4cd
katzhang <arcadychang@gmail.com>
2014-10-05 18:43:06 -0400

    added finding label and get message id based on given label

--- app/scripts/gmail-api.js
+++ app/scripts/gmail-api.js
@@ -6,6 +6,8 @@ var gmailApi = gmailApi || {};
 	var clientId = '481653505775-tvpdbhtbmmpf7fasgnm8o69ro31fou40.apps.googleusercontent.com';
 	var apiKey = 'AIzaSyAH8RDk8TZmMN1LuVdgW1w4ixpJWE94dSg';
 	var scopes = 'https://www.googleapis.com/auth/gmail.readonly';
+	var hunchesLabel = 'hunches';
+	var hunchesLabelId = '';
 
 	gmailApi.auth = function() {
 		var config = {
@@ -27,7 +29,7 @@ var gmailApi = gmailApi || {};
 		console.log('handleAuthResult');
 		if (authResult && !authResult.error) {
 			authorizeButton.style.visibility = 'hidden';
-			gmailApi.makeApiCall();
+			gmailApi.loadApi();
 		} else {
 			authorizeButton.style.visibility = '';
 			authorizeButton.onclick = gmailApi.handleAuthClick;
@@ -39,16 +41,45 @@ var gmailApi = gmailApi || {};
 		return false;
 	}
 
-	gmailApi.makeApiCall = function() {
+	gmailApi.loadApi = function() {
 		gapi.client.load('gmail', 'v1', function() {
-			var request = gapi.client.gmail.users.messages.list({
-				'userId': 'me'
-			})
-
-			request.execute(function(response) {
-				console.log(response);
-			})
+			console.log('gapi.client gmail api loaded');
+			gmailApi.listLabels();
 		})
+	}
+
+	gmailApi.listThreads = function(labelId) {
+		var request = gapi.client.gmail.users.messages.list({
+			'userId': 'me',
+			'labelIds': labelId
+		})
+
+		request.execute(function(response) {
+			console.log(response);
+		})
+	}
+
+	gmailApi.listLabels = function() {
+		var request = gapi.client.gmail.users.labels.list({
+			'userId': 'me'
+		})
+
+		request.execute(function(response) {
+			var labels = response.labels;
+			hunchesLabelId = findId(labels, hunchesLabel);
+			gmailApi.listThreads(hunchesLabelId);
+		})
+	}
+
+	/* Helper Functions */
+	function findId(array, name) {
+		for(var i = 0; i < array.length; i++) {
+			if (array[i].name == name) {
+				return array[i].id;
+			}
+		}
+
+		return false;
 	}
 
 })()
